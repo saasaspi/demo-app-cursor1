132d304eee4222d97ac65b47e2ee9eb328e7798a
saasaspi <jbdurgesh@gmail.com>
2024-09-09 19:50:08 +0530

    new

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="text-4xl font-bold">Hello World</h1>
+      <h2 className="text-xl mt-2">Welcome to our website</h2>
     </main>
   );
 }
